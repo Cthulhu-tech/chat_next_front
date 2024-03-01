aa1ebb6790dd288ddb877989ac784e76761a3de2
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2024-03-01 16:06:32 +0400

    chat fron update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { RouterProvider } from "react-router";
+import { router } from "./routers/routrers";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import './style.css';
+
 export const App = () => {
   
-  return <>app init</>
+  return <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 }
